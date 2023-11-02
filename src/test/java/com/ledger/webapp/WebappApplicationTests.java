eb1a225f62728fbb5f613363c414db1bee5cc09d
kkongji <wisejohn950330@gmail.com>
2023-11-02 11:20:08 +0900

    :tada: Begin:"개발 환경 세팅"
    Backend Java Spring 개발환경세팅

---
+++ src/test/java/com/ledger/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ledger.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
