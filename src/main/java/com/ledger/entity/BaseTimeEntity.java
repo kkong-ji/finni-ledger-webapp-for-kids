e969fc86db84aa29521c80c8dc7e139ffc624e48
unknown <wisejohn950330@gmail.com>
2023-11-14 18:17:39 +0900

    :sparkles: Feat: "Member 기능(회원 생성, 수정, 삭제) 관련 Entity 생성"
    회원과 관련된 기능들을 구현하기 위한 branch

---
+++ src/main/java/com/ledger/entity/BaseTimeEntity.java
@@ -0,0 +1,37 @@
+package com.ledger.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@EntityListeners(value = {AuditingEntityListener.class})        // Auditing을 적용하기 위해서 @EntityListeners 어노테이션을 추가
+@MappedSuperclass                                               // 공통 매핑 정보가 필요할 때 사용하는 어노테이션으로 부모 클래스를 상속 받는
+@Getter                                                         // 자식 클래스에 매핑 정보만 제공
+@Setter
+public abstract class BaseTimeEntity {
+
+    @CreatedDate                                                // 엔티티가 생성되어 저장될 때 시간을 자동으로 저장
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    @LastModifiedDate                                           // 엔티티의 값을 변경할 때 시간을 자동으로 저장
+    private LocalDateTime updateTime;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private String modifiedDate;
+
+    /* 해당 엔티티를 업데이트 하기 이전에 실행*/
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
+    }
+}
---
+++ src/main/java/com/ledger/entity/Member.java
@@ -0,0 +1,60 @@
+package com.ledger.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String password;
+
+    private String nickname;
+
+    private Integer age;
+
+    private String picture;
+
+    private String provider;                    // oauth2를 이용한 소셜 로그인 시 플랫폼
+
+    @Column(name="social_id", unique = true)    // 회원들을 유일하게 식별하는 값이기 때문에 unique = true 값 걸어둠
+    private String providerId;                  // oauth2를 이용한 소셜 로그인 시 아이디값
+
+    @Builder(builderClassName = "OAuth2Register", builderMethodName = "userDetailRegister")
+    public Member(String nickname, String password, String picture, String provider, String providerId) {
+        this.nickname = nickname;
+        this.password = password;
+        this.picture = picture;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+    /**
+     * 회원수정 메소드
+     */
+    public void updateUserNickname(String nickname) { this.nickname = nickname; }
+
+    public void updateUserAge(Integer age) { this.age = age; }
+
+    // 유저 password 변경의 경우, 사용자가 직접 변경할 수는 없지만 로그인 플랫폼이 변경될 경우를 위한 것
+    // ex) 카카오 -> 네이버로 변경. 두 소셜 계정의 비밀번호는 각각 다를 수 있기 때문
+    private void updatePassword(String password) { this.password = password; }
+
+    public void updateUserPicture(String picture) { this.picture = picture; }
+
+    public void updateUserProvider(String provider) { this.provider = provider; }
+
+    // 상기 서술한 updatePassword 메소드와 같은 이유로 private
+    private void updateUserProviderId(String providerId) { this.providerId = providerId; }
+}
