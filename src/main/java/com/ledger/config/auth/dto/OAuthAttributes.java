675db8069ced8e020eb35674f7308c4202374798
unknown <wisejohn950330@gmail.com>
2023-12-02 19:25:43 +0900

    :sparkles: Feat: "OAuth2 소셜 로그인 기능 구현 (Kakao)"
    OAuth2 소셜 로그인 (Naver, Kakao) 모두 구현 완료

--- src/main/java/com/ledger/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/ledger/config/auth/dto/OAuthAttributes.java
@@ -33,6 +33,10 @@ public class OAuthAttributes {
             return ofNaver("id", attributes);
         }
 
+        if("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
+
         return null;
     }
 
@@ -45,6 +49,21 @@ public class OAuthAttributes {
                 .email((String) response.get("email"))
                 .profile((String) response.get("profile_image"))
                 .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        // kakao는 kakao_account에 유저정보가 있다. (email)
+        Map<String, Object> kakaoAccount = (Map<String, Object>)attributes.get("kakao_account");
+        // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+        Map<String, Object> kakaoProfile = (Map<String, Object>)kakaoAccount.get("profile");
+
+        return OAuthAttributes.builder()
+                .nickname((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .profile((String) kakaoProfile.get("profile_image"))
+                .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
