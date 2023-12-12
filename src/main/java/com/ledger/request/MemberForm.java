720a70b75ea0af9fd6b494b81e8d1744abdb2457
StrongCatWill <ssyysskywalker@gmail.com>
2023-12-12 21:26:48 +0900

    :sparkles: Feat : 마이페이지 회원 정보 수정, 회원 탈퇴 기능 구현
    탈퇴 후 세션 처리 논의 필요ㅎ

---
+++ src/main/java/com/ledger/request/MemberForm.java
@@ -0,0 +1,14 @@
+package com.ledger.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+
+    private String nickName;
+    private String email;
+    private int age;
+    private String profile;
+
+}
