c439df1a5d4ca54b40802c353ed13b4f46f25bc6
StrongCatWill <ssyysskywalker@gmail.com>
2023-12-12 21:20:01 +0900

    :wheelchair: Improve : members/ url에서 세션 유지 항목 추가
    추후 세션 작업을 위한 논의 필요함

--- src/main/java/com/ledger/config/SecurityConfig.java
+++ src/main/java/com/ledger/config/SecurityConfig.java
@@ -2,20 +2,15 @@ package com.ledger.config.auth;
 
 import com.ledger.config.auth.CustomOAuth2UserService;
 import com.ledger.service.MemberService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -42,10 +37,12 @@ public class SecurityConfig {
                 .csrf().disable()
                 .httpBasic().disable()
                 .formLogin().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/css/**", "/images/**", "/js/**").permitAll()
+                .antMatchers("/members/**").authenticated()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
